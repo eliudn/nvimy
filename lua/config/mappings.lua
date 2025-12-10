31c337682114cb2147f17c491e68dd1cc1a31901
eliud niebles <eliudniebles@gmail.com>
2025-12-09 20:49:10 -0500

    se agregan los keymaps basico

---
+++ lua/config/mappings.lua
@@ -0,0 +1,8 @@
+vim.keymap.set("n", "<leader>w", "<cmd>w<cr>", { noremap = true })
+vim.keymap.set("n", "<leader>q", "<cmd>q<cr>", { noremap = true })
+vim.keymap.set("n", "<leader>x", "<cmd>x<cr>", { noremap = true })
+
+vim.keymap.set("i", "<C-h>", "<Left>", { noremap = true })
+vim.keymap.set("i", "<C-j>", "<Down>", { noremap = true })
+vim.keymap.set("i", "<C-k>", "<Up>", { noremap = true })
+vim.keymap.set("i", "<C-l>", "<Right>", { noremap = true })
